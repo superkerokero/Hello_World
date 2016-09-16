fdd5c8cd650455b634245ebc69ba4add6824eceb
superkerokero <superkerokero@users.noreply.github.com>
2016-09-16 13:08:30 +0900

    Added test module for trapezoid_gen.

---
+++ test_trapezoid_gen.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+#!/usr/bin/env python
+"""The test module for trapezoid_gen module."""
+
+import unittest
+import trapezoid_gen
+
+class TestModule(unittest.TestCase):
+    """Unittest extend."""
+    def setUp(self):
+        "setup initial conditions for test."
+        init_points = [[0.0, 0.0], [0.0, 2.0], [4.0, 4.0], [4.0, 0.0]]
+        intervals = [1.0, 1.0]
+        max_core = 50
+        overlap = 0.5
+        self.test = trapezoid_gen.Trapezoid(init_points, intervals, max_core, overlap)
+    def tearDown(self):
+        "destructor for test."
+        del self.test
+    def test_checkborder(self):
+        "test the _checkborder function."
+        self.assertTrue(self.test._checkborder([0.0, 2.0]))
+        self.assertFalse(self.test._checkborder([5.0, 4.0]))
+    def test_generate_sets(self):
+        "test generate_sets function."
+        #plus 1 is needed for accounting the endpoints.
+        expected_size = int((self.test.points[2][0] - self.test.points[0][0])/ \
+                self.test.intervals[0]) + 1
+        self.assertEqual(expected_size,len(self.test.generate_sets()[0]))
+        
+        
+#Perform unittest if the script is run directly.       
+if __name__ == "__main__":
+    unittest.main()
--- trapozoid_gen.py
+++ trapozoid_gen.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python
+# coding: utf-8
 
-""" trapozoid_gen.py:
+""" trapezoid_gen.py:
     This is a module used to generate trapozoid points based on
     input: four points defining a trapozoid area inside the sampling space.
            (type: tuple of tuples)"""
 
-import unittest
 
 class Trapezoid(object):
     "Class for the trapezoid generation."
-    def __init__(self, init_points):
+    def __init__(self, init_points, intervals, max_core, overlap):
         """Constructor of the class(Trapezoid_gen).
            The numberings of points (x,y) in the trapezoid are given as below.
            y
@@ -20,19 +19,70 @@ class Trapezoid(object):
            1----------------------------4 ===== x"""
 
         self.points = init_points
+        self.intervals = intervals
+        self.max_core = max_core
+        self.overlap = overlap
         try:
             self.slope = (self.points[2][1]-self.points[1][1]) \
                          /(self.points[2][0]-self.points[1][0])
         except ZeroDivisionError:
-            print "The slope has a zero division error, \
-                   please check the input points"
+            print "The slope has a zero division error, " + \
+                  "please check the input points"
     def _checkborder(self, point):
-        """Check whether a point is inside the border."""
+        "Check whether a point is inside the border."
         bottom = (point[1] >= self.points[0][1])
         left = (point[0] >= self.points[0][0])
         right = (point[0] <= self.points[3][0])
-        border = ((point[1]-self.points[1][1]) >= self.slope* \
+        border = ((point[1]-self.points[1][1]) <= self.slope* \
                   (point[0]-self.points[1][0]))
-        return bottom and left and right and border
-    def _normalize(self)
-    
+        return (bottom and left and right and border)
+    def _init_dict(self, start, end, interval):
+        """Initialize dict with all keys ranging from start to end 
+           having initial value of 0."""
+        idict = {}
+        temp = start
+        while temp <= end:
+            idict[temp] = set()
+            temp += interval
+        return idict
+    def generate_sets(self):
+        "The function that generates trapezoid sets."
+        print "Start generating trapezoid points based on input data."
+        #generate empty dict for 1st & 2nd coordinates.
+        dict_x = self._init_dict(self.points[0][0], self.points[3][0], \
+                self.intervals[0])
+        dict_y = self._init_dict(self.points[0][1], self.points[2][1], \
+                self.intervals[1])
+        #add the bottom-left point to dicts.
+        num_core = 1
+        #list = in python is call-by-reference.
+        #in order to copy into a new list that is irrelevant to the
+        #original one, you can use either:
+            #current_point = list(self.points[0])
+            #or as below.
+        current_point = self.points[0][:]
+        dict_x[current_point[0]].add(num_core)
+        dict_y[current_point[1]].add(num_core)
+        while num_core < self.max_core:
+            num_core += 1
+            current_point[1] += self.intervals[1]
+            if self._checkborder(current_point):
+                #if the updated point is within border, add directly.
+                dict_x[current_point[0]].add(num_core) 
+                dict_y[current_point[1]].add(num_core)
+            else:
+                #if the updated point exceeds border, update x value 
+                #and reset y value.
+                current_point[0] += self.intervals[0]
+                current_point[1] = self.points[0][1]
+                if self._checkborder(current_point):
+                    dict_x[current_point[0]].add(num_core) 
+                    dict_y[current_point[1]].add(num_core)
+                else:
+                    print current_point
+                    print "CAUTION: " + \
+                    "Raw list generation completed without reaching " + \
+                    "maximum core number: generated %d points" % num_core
+                    break
+        print "Raw list generation completed."
+        return [dict_x, dict_y]
